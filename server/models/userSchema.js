8bbc5f08b95fc4aaa8073b21e77e09976b07b67c
Subham <subhamtndkr9@gmail.com>
2023-05-19 13:31:34 +0545

    commit

--- server/models/userSchema.js
+++ server/models/userSchema.js
@@ -19,6 +19,10 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    status: {
+      type: String,
+      default: "1",
+    },
     // image: {
     //   type: String,
     //   required: true,
